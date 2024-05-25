6bb8ce467fc1174f755cb776061b81b2b50d0640
hayeonn2 <dwd1205@naver.com>
2024-05-25 23:02:53 +0900

    feat: about.jsx 를 통해 페이지 이동 테스트

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
@@ -13,27 +6,7 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <Link to="/about">about</Link>
         </li>
       </ul>
     </div>
---
+++ app/routes/about.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div>야 리믹스 나 한번 시작해볼게?</div>;
+}
