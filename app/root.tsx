933097743991349c192417c0f3ed7a092aa02055
hayeonn2 <dwd1205@naver.com>
2024-05-25 23:40:48 +0900

    test : 다크모드 테스트 중인데 잘 안됨

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./routes/theme";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,5 +27,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Outlet />
+    </ChakraProvider>
+  );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+
       <ul>
         <li>
           <Link to="/about">about</Link>
