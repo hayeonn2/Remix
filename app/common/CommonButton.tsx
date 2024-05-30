a1516588d33fa41a2481bc6edcc292020517f81b
hayeonn2 <dwd1205@naver.com>
2024-05-30 17:29:39 +0900

    feat: CommonButton 컴포넌트 그라디언트 props 추가

--- app/common/CommonButton.tsx
+++ app/common/CommonButton.tsx
@@ -1,73 +1,19 @@
-// import type { SxProps, Theme } from '@mui/material';
-// import { Button } from '@mui/material';
-// import {
-//   deepPurple, indigo, purple, red,
-// } from '@mui/material/colors';
-// import { styled } from '@mui/material/styles';
-
-// type GradientButtonProps = {
-//   colors?: string[];
-// }
-
-// const GradientButton = styled(Button)<GradientButtonProps>(({ colors }) => ({
-//   color: 'white',
-//   ...(colors && {
-//     background: `linear-gradient(to right bottom, ${colors.join(',')})`,
-//   }),
-// }));
-
-// const CommonButton = ({
-//   variant = 'contained',
-//   color = 'primary',
-//   size = 'medium',
-//   disabled = false,
-//   startIcon,
-//   endIcon,
-//   onClick,
-//   sx,
-//   children,
-// }: CommonButtonProps) => {
-//   return (
-//     <Button
-//       variant={variant}
-//       color={color}
-//       size={size}
-//       disabled={disabled}
-//       startIcon={startIcon}
-//       endIcon={endIcon}
-//       onClick={onClick}
-//       sx={{
-//         borderRadius: '9999px', ...sx,
-//       }}
-//     >
-//       {children}
-//       <GradientButton
-//         variant="contained"
-//         colors={[
-//           red[50],
-//           deepPurple[500],
-//           purple[500],
-//         ]}
-//       >
-//         ㅇ우앙ㅂ
-//       </GradientButton>
-//     </Button>
-//   );
-// };
 
 import type { SxProps, Theme } from '@mui/material';
-import { Button } from '@mui/material';
+import { Button, useTheme  } from '@mui/material';
+
+import type { ExtendedPaletteOptions  } from '~/themes/theme';
 
 interface CommonButtonProps {
   variant?: 'text' | 'contained' | 'outlined';
-  color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+  color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'| 'other' | 'primaryLight';
   size?: 'small' | 'medium' | 'large';
   disabled?: boolean;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   sx?: SxProps<Theme>;
-  gradient?: string[];
+  gradient?: string | string[];
   children: React.ReactNode;
 }
 
@@ -82,7 +28,28 @@ const CommonButton = ({
   sx,
   gradient,
   children,
+  ...props
 }: CommonButtonProps) => {
+  const theme = useTheme() as Theme & { palette: ExtendedPaletteOptions };
+
+  const getGradient = () => {
+    if (Array.isArray(gradient)) {
+      // gradient prop이 배열일 때, 직접 색상 값을 넣어 적용
+      return `linear-gradient(to bottom, ${gradient.join(',')})`;
+    }
+
+    switch (gradient) {
+      case 'primaryMain':
+        return `linear-gradient(to bottom, ${theme.palette.primary.top}, ${theme.palette.primary.low})`;
+      case 'primaryLight':
+        return `linear-gradient(to bottom, ${theme.palette.primaryLight?.top}, ${theme.palette.primaryLight?.low})`;
+      case 'primaryDark':
+        return `linear-gradient(to bottom, ${theme.palette.primaryDark?.top}, ${theme.palette.primaryDark?.low})`;
+      default:
+        return '';
+    }
+  };
+
   return (
     <Button
       variant={variant}
@@ -93,13 +60,10 @@ const CommonButton = ({
       endIcon={endIcon}
       onClick={onClick}
       sx={{
-        borderRadius: '9999px',
         ...sx,
-        ...(gradient && {
-          background: `linear-gradient(to bottom, ${gradient.join(',')})`,
-          color: 'white',
-        }),
+        background: getGradient(),
       }}
+      {...props}
     >
       {children}
     </Button>
